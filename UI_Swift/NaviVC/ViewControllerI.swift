9c132f30a6cf4e85fef3985867d9e732861c60bd
huang <huangfei_07@163.com>
2021-08-11 19:49:55 +0800

    添加自定义Window入口；

---
+++ UI_Swift/NaviVC/ViewControllerI.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerI.swift
+//  UI_Swift
+//
+//  Created by PxLand on 2021/8/11.
+//  Copyright © 2021 nutletor. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerI: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UI_Swift/NaviVC/ViewControllerIII.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerIII.swift
+//  UI_Swift
+//
+//  Created by PxLand on 2021/8/11.
+//  Copyright © 2021 nutletor. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerIII: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
